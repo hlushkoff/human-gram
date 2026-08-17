81b7b2282cfe636e5e34b38f7b4370fe6fddee4d
hlushkoff <aleh.hlushkou@gmail.com>
2026-08-17 11:40:01 +0200

    chore(rebrand): HumanGram header/branding batch 19

--- app/src/main/java/org/thunderdog/challegram/unsorted/Settings.java
+++ app/src/main/java/org/thunderdog/challegram/unsorted/Settings.java
@@ -1,5 +1,5 @@
 /*
- * This file is a part of Telegram X
+ * This file is a part of HumanGram
  * Copyright © 2014 (tgx-android@pm.me)
  *
  * This program is free software: you can redistribute it and/or modify
